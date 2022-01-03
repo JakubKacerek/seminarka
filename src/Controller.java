620a8c4c8c88fbdfe96b49c224cedefb016c4425
Jakub Káčerek <jakub.kacerek@tul.cz>
2022-01-03 22:37:22 +0100

    Delete src directory

--- src/Controller.java
+++
@@ -1,9 +0,0 @@
-
-public class Controller {
-
-    public static Points[] objects = new Points[1000];
-
-    public static void main(String[] args){
-        Text_Controlls.Empty(objects);
-    }
-}
